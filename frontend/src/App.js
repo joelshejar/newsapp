99b4030ada155a3ad42598ca7f6c32fd4095a7bf
joelshejar <joel.rajesh13@gmail.com>
2022-12-24 23:57:01 +0530

    Added css resets along with ggogle fonts and a basic layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,94 @@
-import logo from "./logo.svg";
 import "./styles/App.scss";
+import logo from "./logo.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <>
+      <header className="header">
+        <p className="header--para">
+          Magazine and newspaper with news around the world
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
-    </div>
+      <div className="page">
+        <div className="layout">
+          <article>
+            <div>
+              <div className="news1">jomin</div>
+              <img
+                className="image1"
+                src="https://c.pxhere.com/photos/e6/c5/model_fashion_girl_fashion_models_fashion_model_young_woman_hair_fashion_model-1369002.jpg!d"
+              />
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Laudantium, ipsa consequatur? Rerum tempore esse pariatur
+                molestiae! Saepe dicta vero quis earum, natus atque nobis quia
+                labore, culpa deleniti eaque perspiciatis?
+              </p>
+            </div>
+          </article>
+          <article>
+            <div>
+              <div className="news1">jomin</div>
+              <img
+                className="image1"
+                src="https://c.pxhere.com/photos/e6/c5/model_fashion_girl_fashion_models_fashion_model_young_woman_hair_fashion_model-1369002.jpg!d"
+              />
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Laudantium, ipsa consequatur? Rerum tempore esse pariatur
+                molestiae! Saepe dicta vero quis earum, natus atque nobis quia
+                labore, culpa deleniti eaque perspiciatis?
+              </p>
+            </div>
+          </article>
+          <article>
+            <div>
+              <div className="news1">jomin</div>
+              <img
+                className="image1"
+                src="https://c.pxhere.com/photos/e6/c5/model_fashion_girl_fashion_models_fashion_model_young_woman_hair_fashion_model-1369002.jpg!d"
+              />
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Laudantium, ipsa consequatur? Rerum tempore esse pariatur
+                molestiae! Saepe dicta vero quis earum, natus atque nobis quia
+                labore, culpa deleniti eaque perspiciatis?
+              </p>
+            </div>
+          </article>
+          <article>
+            <div>
+              <div className="news1">jomin</div>
+              <img
+                className="image1"
+                src="https://c.pxhere.com/photos/e6/c5/model_fashion_girl_fashion_models_fashion_model_young_woman_hair_fashion_model-1369002.jpg!d"
+              />
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Laudantium, ipsa consequatur? Rerum tempore esse pariatur
+                molestiae! Saepe dicta vero quis earum, natus atque nobis quia
+                labore, culpa deleniti eaque perspiciatis?
+              </p>
+            </div>
+          </article>
+          <article>
+            <div>
+              <div className="news1">jomin</div>
+              <img
+                className="image1"
+                src="https://c.pxhere.com/photos/e6/c5/model_fashion_girl_fashion_models_fashion_model_young_woman_hair_fashion_model-1369002.jpg!d"
+              />
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Laudantium, ipsa consequatur? Rerum tempore esse pariatur
+                molestiae! Saepe dicta vero quis earum, natus atque nobis quia
+                labore, culpa deleniti eaque perspiciatis?
+              </p>
+            </div>
+          </article>
+        </div>
+      </div>
+    </>
   );
 }
 
